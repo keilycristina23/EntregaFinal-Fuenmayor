7f5dd3988d9601626057a75f8e14f7a3988d002e
Keily <keily.cristina2360@gmail.com>
2022-11-29 12:13:15 -0300

    Primercommit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import {componentesNav} from './components/navBar/navBar'
+//import './App.css'
+
+export function App() {
+
+  return (
+<componentesNav />  
+  )
+}
+
---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logoSkin from '../../assets/Logoskin.svg'
+import './navBar.css'
+
+
+export const componentesNav = () => {
+
+  const MostrarMensaje = () => {
+    alert('le diste al home');
+  } 
+
+  
+  return (
+
+    <>
+  <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">SKINSTUDIO</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">TRATAMIENTOS</Nav.Link>
+            <Nav.Link href="#features">E-SHOP</Nav.Link>
+            <Nav.Link href="#pricing">CONTACTO</Nav.Link>
+            <Nav.Link href="#pricing">UBICACION</Nav.Link>
+            <cartWidget/>
+
+          </Nav>
+     
+        </Container>
+      </Navbar>
+
+      
+          <div className='logo'>
+            <img src={logoSkin}></img>
+          </div>
+      
+
+
+    </>
+  
+  )
+}
