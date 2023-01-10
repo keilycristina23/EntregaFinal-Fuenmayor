a1fa772c21f35c9512856eb4711161c76c2b3ac3
Keily <keily.cristina2360@gmail.com>
2023-01-10 12:13:21 -0300

    context002

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -3,17 +3,19 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Input from "../Input/Input";
 import { useCartContext } from "../contexts/CartContext";
-
+import { useState } from "react";
 export const ItemDetail = ({ producto }) => {
   console.log(producto);
 
+  const [cantidad, setCantidad] = useState(0);
   const { agregarCarrito, cartList } = useCartContext();
 
   const onAdd = (cantidad) => {
     alert(`cantidad de productos seleccionado:${cantidad}`);
-    agregarCarrito({ ...producto, cantidad });
+    agregarCarrito(producto, cantidad);
   };
   console.log(cartList);
+
   return (
     <div style={{ width: "18rem", margin: "1rem" }}>
       <Card style={{ width: "18rem" }}>
@@ -26,14 +28,17 @@ export const ItemDetail = ({ producto }) => {
           <ListGroup.Item>{producto.duracion}</ListGroup.Item>
           <ListGroup.Item>{producto.precio}</ListGroup.Item>
         </ListGroup>
-        <Card.Body>
-          <Card.Link>
-            <ItemCount />
-          </Card.Link>
-        </Card.Body>
+        {cantidad === 0 ? (
+          <Card.Body>
+            <Card.Link>
+              <ItemCount stock={6} initial={1} onAdd={onAdd} />
+            </Card.Link>
+          </Card.Body>
+        ) : (
+          <link to={"cart"}>Ir al carrito </link>
+        )}
       </Card>
       <Input />
-      <ItemCount stock={6} initial={1} onAdd={onAdd} />
     </div>
   );
 };
--- src/components/contexts/CartContext.jsx
+++ src/components/contexts/CartContext.jsx
@@ -4,13 +4,29 @@ export const CartContext = createContext([]);
 
 export const useCartContext = () => useContext(CartContext);
 
+const isInCart = (id) => {
+  return cartList.some((el) => el.id === id);
+};
+
 export const CartContextProvider = ({ children }) => {
   const [cartList, setCartList] = useState([]);
 
-  const agregarCarrito = (producto) => {
-    setCartList([...cartList, producto]);
+  const agregarCarrito = (producto, cantidad) => {
+    const newObj = {
+      ...producto,
+      cantidad,
+    };
+    if (isInCart(newObj.id)) {
+      cartList.map((el) => {
+        if (el.id === newObj.id) {
+          el.cantidad += newObj.cantidad;
+        }
+        return el;
+      });
+    } else {
+      setCartList([...cartList, newObj]);
+    }
   };
-
   return (
     <CartContext.Provider
       value={{
